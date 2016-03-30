2299b494959dbd138ba6206154ca00af3259a25c
Jule <julischka@onlinehome.de>
2016-03-30 21:33:02 +0200

    target ignore und anderer rest service pfad

--- src/main/java/org/jboss/as/quickstarts/kitchensink/util/Constants.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/util/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 	public static final String PROTOCOL = "https://";
 	public static final String MAIN_URL = PROTOCOL + "masalis.de/teamPlanner";
 	public static final String LOGIN_URL = MAIN_URL + "/login.php";
-	public static final String SERVICE_URL = PROTOCOL + "jbosswildfly-datacollection.rhcloud.com/rest";
-	public static final String ACTIVATION_URL = MAIN_URL + "/activate";
+	public static final String SERVICE_URL = PROTOCOL + "teamplaner-datacollection.rhcloud.com/rest";
+	public static final String ACTIVATION_URL = MAIN_URL + "/php/activate.php";
 	public static final String TRAINER_ROLE = "Trainer";
 	public static final String SPIELER_ROLE = "Spieler";
 
