5edfbd1def04f0b5bb68b34ea94c9f78db4fb376
Jule <julischka@onlinehome.de>
2016-05-06 22:01:42 +0200

    Weekly Mail am Sonntag

--- src/main/java/de/masalis/teamplanner/timer/WeeklyTimer.java
+++ src/main/java/de/masalis/teamplanner/timer/WeeklyTimer.java
@@ -50,7 +50,7 @@ public class WeeklyTimer {
 	@Inject
     private Logger log;
 	
-	@Schedule(hour = "0", minute = "0", second = "0")
+	@Schedule(dayOfWeek = "Sun", hour = "0", minute = "0", second = "0")
 	public void sendWeeklyTeamNotification() {
 		List<User> users = userService.findAllWeeklyNotifiedUsers();
 		if(users != null){
