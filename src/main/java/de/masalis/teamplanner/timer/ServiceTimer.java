bee7f4f67e5106c5e4e6624a1607d1349ee6ccb4
Adrian <adrian_fiedler@msn.com>
2016-07-03 22:23:28 +0200

    Remove ServiceTimer for hash

--- src/main/java/de/masalis/teamplanner/timer/ServiceTimer.java
+++ src/main/java/de/masalis/teamplanner/timer/ServiceTimer.java
@@ -1,24 +1,11 @@
 package de.masalis.teamplanner.timer;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.annotation.Resource;
-import javax.ejb.EJBContext;
-import javax.ejb.Schedule;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.xml.bind.DatatypeConverter;
-
-import org.jboss.as.quickstarts.kitchensink.model.User;
-import org.jboss.as.quickstarts.kitchensink.service.UserService;
-import org.jboss.as.quickstarts.kitchensink.util.CipherUtil;
 
 @Stateless
 public class ServiceTimer {
 
+	/*
 	@Inject
 	UserService userService;
 	
@@ -27,7 +14,6 @@ public class ServiceTimer {
 
     @Resource
     private EJBContext context;
-
 	@Schedule(hour = "*", minute = "5", second = "0")
 	public void serviceTimer() {
 		List<User> allUsers = userService.findAllUsers();
@@ -43,8 +29,8 @@ public class ServiceTimer {
 				String[] splits = passwordhashAndSalt.split(":");
 				if(splits.length == 2 && passwordhashAndSalt.indexOf(":") == 128 && splits[0].length() == 128 && splits[1].length() == 24){
 		    		// nichts machen
-					String test = CipherUtil.getHashedPassword("test", DatatypeConverter.parseBase64Binary(splits[1]));
-					log.log(Level.INFO, "same: "+(test.equals(splits[0])? "true" : "false"));
+					// String test = CipherUtil.getHashedPassword("test", DatatypeConverter.parseBase64Binary(splits[1]));
+					// log.log(Level.INFO, "same: "+(test.equals(splits[0])? "true" : "false"));
 		    	} else{
 		    		//passwort hashen
 		    		String neuesPw = CipherUtil.createHashedPasswordWithSaltAppended(passwordhashAndSalt);
@@ -58,4 +44,6 @@ public class ServiceTimer {
 			ex.printStackTrace();
 		}
 	}
+	
+	*/
 }
